eb97df74481f949c19d45688d39c597605dfb4e7
ankitverma0202 <156880857+ankitverma0202@users.noreply.github.com>
2024-01-21 17:29:48 +0530

    Add files via upload

---
+++ asstes/javascript/style.js
@@ -0,0 +1,15 @@
+function nextPageData(){
+    
+}
+
+
+
+function whatsappData(){
+//  location.href='https://wa.me/918949004273/?text=Hello Umesh sir;';
+}
+
+function telegramData(){
+
+}
+
+AOS.init();
